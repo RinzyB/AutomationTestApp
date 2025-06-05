2068a1dadba188ca4f63b1568be8ce11a02a6e0f
Bhutia <Bhutia@rinz.com>
2025-06-05 12:53:06 -0700

    adding the constants env file

---
+++ src/test/java/org/test/util/Constants.java
@@ -0,0 +1,39 @@
+package org.test.util;
+
+public class Constants {
+	public final static String screenshotsFolder = "path\\AutomationTestApp\\output\\screenshots\\";
+
+	public final static String excelDataFile = "path\\AutomationTestApp\\src\\main\\resources\\HealthCareTestApp.xlsx";
+
+	// Database configs
+	public final static String dbURL = "jdbc:mysql://localhost:3306/healthcaremgmt";
+	public final static String dbUser = "dbuser";
+	public final static String dbPass = "dbpass";
+	public final static String query = "SELECT * FROM healthcaremgmt.logininfo where id=3;";
+
+	public final static String curaURL = "https://katalon-demo-cura.herokuapp.com/";
+	public final static String curaDataSheetName = "CURA Healthcare";
+
+	public final static String authURL = "@the-internet.herokuapp.com/basic_auth";
+	public final static String brokenImageURL = "https://the-internet.herokuapp.com/broken_images";
+	public final static String fileupdownloadURL = "https://blueimp.github.io/jQuery-File-Upload/";
+	public final static String downloadPath = "path\\Selenium\\TestDownload\\";
+	
+	public final static String lambdaTestWindow = "https://www.lambdatest.com/selenium-playground/window-popup-modal-demo";
+	public final static String lambdaTestiFrame = "https://www.lambdatest.com/selenium-playground/iframe-demo/";
+	public final static String lambdaTestDragDrop = "https://www.lambdatest.com/selenium-playground/drag-and-drop-demo";
+	public final static String lambdaTestNestedIFrames = "https://www.lambdatest.com/selenium-playground/nested-frames/";
+	public final static String lambdaTestHover = "https://www.lambdatest.com/selenium-playground/hover-demo";
+	public final static String lambdaTestSimpleForm = "https://www.lambdatest.com/selenium-playground/simple-form-demo";
+	public final static String lambdaTestDualList = "https://www.lambdatest.com/selenium-playground/bootstrap-dual-list-box-demo";
+	public final static String lambdaTestFileDownload = "https://www.lambdatest.com/selenium-playground/generate-file-to-download-demo";
+	public final static String lambdaTestTableSort = "https://www.lambdatest.com/selenium-playground/table-sort-search-demo";
+	
+	public final static String username = "user.name";
+	public final static String accesskey = "access_key";
+	public final static String gridURL = "@hub.lambdatest.com/wd/hub";
+	public final static String selGridURL = "http://ip:4444";
+	public final static String reqResAPI_baseURI = "https://reqres.in/";
+	public final static String cheapoair = "https://www.cheapoair.com/flights/booknow/cheap-flight-tickets";
+	public static final String practiceForm = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+}
